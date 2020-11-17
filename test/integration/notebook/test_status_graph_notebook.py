68fc47435ddf05f8840d1376efa41a296a6c9614
Austin Kline <akline@amazon.com>
2020-11-17 11:30:03 -0800

    rename test directory to avoid conflicts

--- test/integration/notebook/test_status_graph_notebook.py
+++ test/integration/notebook/test_status_graph_notebook.py
@@ -3,7 +3,7 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 """
 
-from test.integration.graph_notebook.GraphNotebookIntegrationTest import GraphNotebookIntegrationTest
+from test.integration.notebook.GraphNotebookIntegrationTest import GraphNotebookIntegrationTest
 
 
 class TestGraphMagicStatus(GraphNotebookIntegrationTest):
