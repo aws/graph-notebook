d42d4e05f289decd69d880a2eb0d97c36862b785
Austin Kline <akline@amazon.com>
2020-11-18 11:42:50 -0800

    GraphNotebookIntegrationTest derives from Integration Test

--- test/integration/notebook/GraphNotebookIntegrationTest.py
+++ test/integration/notebook/GraphNotebookIntegrationTest.py
@@ -3,17 +3,26 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 """
 
-
 import json
 
-from test.unit.graph_magic.GraphNotebookTest import GraphNotebookTest
+from IPython import get_ipython
+from IPython.terminal.interactiveshell import TerminalInteractiveShell
+
+from test.integration import IntegrationTest
 
 
-class GraphNotebookIntegrationTest(GraphNotebookTest):
+class GraphNotebookIntegrationTest(IntegrationTest):
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
 
     def setUp(self) -> None:
         super().setUp()
+
+        ip = get_ipython()
+        if ip is None:
+            ip = TerminalInteractiveShell().instance()
+        self.ip = ip
+
+        self.ip.magic('load_ext graph_notebook.magics')
         self.ip.run_cell_magic('graph_notebook_config', '', json.dumps(self.config.to_dict()))
