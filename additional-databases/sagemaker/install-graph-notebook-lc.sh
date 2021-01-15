6c7b1544f4d92445e94719e20b3f7858c447d060
Joy Wang <73913166+joywa@users.noreply.github.com>
2021-01-14 23:06:21 -0700

    Add files via upload

---
+++ additional-databases/sagemaker/install-graph-notebook-lc.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+sudo -u ec2-user -i <<'EOF'
+echo "export GRAPH_NOTEBOOK_AUTH_MODE=DEFAULT" >> ~/.bashrc  # set to IAM instead of DEFAULT if cluster is IAM enabled
+echo "export GRAPH_NOTEBOOK_HOST=CHANGE-ME" >> ~/.bashrc
+echo "export GRAPH_NOTEBOOK_PORT=8182" >> ~/.bashrc
+echo "export NEPTUNE_LOAD_FROM_S3_ROLE_ARN=" >> ~/.bashrc
+echo "export AWS_REGION=us-west-2" >> ~/.bashrc
+source activate JupyterSystemEnv
+echo "installing Python 3 kernel"
+python3 -m ipykernel install --sys-prefix --name python3 --display-name "Python 3"
+echo "intalling python dependencies..."
+pip uninstall NeptuneGraphNotebook -y # legacy uninstall when we used to install from source in s3
+pip install --upgrade graph-notebook
+echo "installing nbextensions..."
+python -m graph_notebook.nbextensions.install
+echo "installing static resources..."
+python -m graph_notebook.static_resources.install
+echo "enabling visualization..."
+jupyter nbextension install --py --sys-prefix graph_notebook.widgets
+jupyter nbextension enable  --py --sys-prefix graph_notebook.widgets
+mkdir -p ~/SageMaker/Neptune
+cd ~/SageMaker/Neptune || exit
+python -m graph_notebook.notebooks.install
+chmod -R a+rw ~/SageMaker/Neptune/*
+source ~/.bashrc || exit
+HOST=${GRAPH_NOTEBOOK_HOST}
+PORT=${GRAPH_NOTEBOOK_PORT}
+AUTH_MODE=${GRAPH_NOTEBOOK_AUTH_MODE}
+SSL=${GRAPH_NOTEBOOK_SSL}
+IAM_CREDENTIALS_PROVIDER=${GRAPH_NOTEBOOK_IAM_PROVIDER}
+LOAD_FROM_S3_ARN=${NEPTUNE_LOAD_FROM_S3_ROLE_ARN}
+if [[ ${SSL} -eq "" ]]; then
+  SSL="True"
+fi
+echo "Creating config with
+HOST:                       ${HOST}
+PORT:                       ${PORT}
+AUTH_MODE:                  ${AUTH_MODE}
+SSL:                        ${SSL}
+IAM_CREDENTIALS_PROVIDER:   ${IAM_CREDENTIALS_PROVIDER}
+AWS_REGION:                 ${AWS_REGION}"
+/home/ec2-user/anaconda3/envs/JupyterSystemEnv/bin/python -m graph_notebook.configuration.generate_config \
+  --host "${HOST}" \
+  --port "${PORT}" \
+  --auth_mode "${AUTH_MODE}" \
+  --ssl "${SSL}" \
+  --iam_credentials_provider "${IAM_CREDENTIALS_PROVIDER}" \
+  --load_from_s3_arn "${LOAD_FROM_S3_ARN}" \
+  --aws_region "${AWS_REGION}"
+source /home/ec2-user/anaconda3/bin/deactivate
+echo "done."
+EOF
