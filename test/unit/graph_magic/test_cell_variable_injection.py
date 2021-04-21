3d34c51e925c5b54a323472cf20e4a9776f9a8e6
Michael Chin <chnmch@amazon.com>
2021-04-20 19:47:22 -0700

    Rewrite unit tests

--- test/unit/graph_magic/test_cell_variable_injection.py
+++ test/unit/graph_magic/test_cell_variable_injection.py
@@ -5,73 +5,76 @@ SPDX-License-Identifier: Apache-2.0
 
 import unittest
 
-from graph_notebook.magics.graph_magic import inject_vars_into_query
+from graph_notebook.decorators.decorators import magic_variables
 
 
 class TestCellVariableInjectionFunction(unittest.TestCase):
-
     def test_gremlin_variable_injection(self):
+        @magic_variables
+        def gremlin_mock(self_var, line, cell, local_ns: dict = None):
+            return cell
+
         cell_expected = "g.V().hasLabel('airport').has('code','CZM').out('route').path().by('code')"
 
-        test_ns = {'a': 'airport', 'c': 'code', 'r': 'route'}
-        test_cell = "g.V().hasLabel('${a}').has('${c}','CZM').out('${r}').path().by('${c}')"
-        new_cell = inject_vars_into_query(test_cell, test_ns)
-
-        self.assertEqual(new_cell, cell_expected)
+        self.assertEqual(gremlin_mock('', '', "g.V().hasLabel('${a}').has('${c}','CZM').out('${r}').path().by('${c}')",
+                                      local_ns={'a': 'airport', 'c': 'code', 'r': 'route'}), cell_expected)
 
     def test_invalid_gremlin_variable_injection(self):
-        expected = None
+        @magic_variables
+        def gremlin_mock(self_var, line, cell, local_ns: dict = None):
+            return cell
 
-        test_ns = {'a': 'airport', 'c': 'code', 'r': 'route'}
-        test_cell = "g.V().hasLabel('${a}').has('${b}','CZM').out('${r}').path().by('${c}')"
-        new_cell = inject_vars_into_query(test_cell, test_ns)
-
-        self.assertEqual(new_cell, expected)
-
-    def test_sparql_variable_injection(self):
-        cell_expected = '''
-                    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                    PREFIX so: <https://schema.org/>
-
-                    SELECT ?city
-                    WHERE {
-                        ?s a so:City .
-                        ?s rdfs:label ?city
-                        FILTER contains(?city,"ou")
-                    }
-                    '''
-
-        test_ns = {'city_prefix': 'ou'}
-        test_cell = '''
-                    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                    PREFIX so: <https://schema.org/>
-
-                    SELECT ?city
-                    WHERE {
-                        ?s a so:City .
-                        ?s rdfs:label ?city
-                        FILTER contains(?city,"${city_prefix}")
-                    }
-                    '''
-        new_cell = inject_vars_into_query(test_cell, test_ns)
-
-        self.assertEqual(new_cell, cell_expected)
-
-    def test_invalid_sparql_variable_injection(self):
         cell_expected = None
 
-        test_ns = {'city_prefix': 'ou'}
-        test_cell = '''
-                    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                    PREFIX so: <https://schema.org/>
+        self.assertEqual(gremlin_mock('', '', "g.V().hasLabel('${a}').has('${b}','CZM').out('${r}').path().by('${c}')",
+                                      local_ns={'a': 'airport', 'c': 'code', 'r': 'route'}), cell_expected)
 
-                    SELECT ?city
-                    WHERE {
-                        ?s a so:City .
-                        ?s rdfs:label ?city
-                        FILTER contains(?city,"${not_a_city_prefix}")
-                    }
-                    '''
-        new_cell = inject_vars_into_query(test_cell, test_ns)
+    def test_sparql_variable_injection(self):
+        @magic_variables
+        def sparql_mock(self_var, line, cell, local_ns: dict = None):
+            return cell
 
-        self.assertEqual(new_cell, cell_expected)
+        cell_expected = '''
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        PREFIX so: <https://schema.org/>
+
+                        SELECT ?city
+                        WHERE {
+                            ?s a so:City .
+                            ?s rdfs:label ?city
+                            FILTER contains(?city,"ou")
+                        }
+                        '''
+
+        self.assertEqual(sparql_mock('', '',
+                        '''
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        PREFIX so: <https://schema.org/>
+
+                        SELECT ?city
+                        WHERE {
+                            ?s a so:City .
+                            ?s rdfs:label ?city
+                            FILTER contains(?city,"${city_prefix}")
+                        }
+                        ''', local_ns={'city_prefix': 'ou'}), cell_expected)
+
+    def test_invalid_sparql_variable_injection(self):
+        @magic_variables
+        def sparql_mock(self_var, line, cell, local_ns: dict = None):
+            return cell
+
+        cell_expected = None
+
+        self.assertEqual(sparql_mock('', '',
+                        '''
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        PREFIX so: <https://schema.org/>
+
+                        SELECT ?city
+                        WHERE {
+                            ?s a so:City .
+                            ?s rdfs:label ?city
+                            FILTER contains(?city,"${not_a_city_prefix}")
+                        }
+                        ''', local_ns={'city_prefix': 'ou'}), cell_expected)
