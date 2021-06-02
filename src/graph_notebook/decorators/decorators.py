648234f18ee9a044f42b713423b31595f83a106c
Michael Chin <chnmch@amazon.com>
2021-06-01 20:57:01 -0700

    Support dictionary value access in variable injection

--- src/graph_notebook/decorators/decorators.py
+++ src/graph_notebook/decorators/decorators.py
@@ -16,6 +16,39 @@ from requests import HTTPError
 
 error_template = retrieve_template("error.html")
 
+check_if_dict_access_regex = re.compile(r'^[a-zA-Z0-9_]+((\[\'.*?\'\])|(\[\".*?\"\]))+$')
+dict_name_regex = re.compile(r'^[^\[]*')
+
+
+def get_variable_injection_value(raw_var: str, local_ns: dict):
+    # check if var string is trying to access a dict
+    if re.match(check_if_dict_access_regex, raw_var):
+        # get the name of the dict
+        dict_name = dict_name_regex.match(raw_var).group(0)
+        print(f'dict_name: {dict_name}')
+        # outer try/except statement in use_magic_variable should catch case where dict_name isn't in local_ns
+        current_dict = local_ns[dict_name]
+        print(f'current_dict:{current_dict}')
+        # get the rest of the string, containing all the nested keys
+        keys_raw = raw_var[len(dict_name):len(raw_var)]
+        print(f'keys_raw: {keys_raw}')
+        # ensure that all the keys use single quotes before we str.split
+        keys_raw = keys_raw.replace('"', "'")
+        print(f'keys_raw with single quotes: {keys_raw}')
+        keys_list = keys_raw[2:(len(keys_raw)-2)].split("']['")
+        print(f'keys_list: {keys_list}')
+        # loop through the nested keys/values until we get the final value
+        for key in keys_list:
+            current_dict = current_dict[key]
+        final_value = json.dumps(current_dict) if type(current_dict) is dict else str(current_dict)
+        return final_value
+    else:
+        final_value = local_ns[raw_var]
+        if type(final_value) is dict:
+            return json.dumps(final_value)
+        else:
+            return str(final_value)
+
 
 def display_exceptions(func):
     @functools.wraps(func)
@@ -64,7 +97,8 @@ def magic_variables(func):
             # args[1] = variable_regex.sub(lambda m: str(local_ns[m.group(1)]), line_string)
             if len(args) > 2:
                 cell_string = args[2]
-                args[2] = variable_regex.sub(lambda m: json.dumps(local_ns[m.group(1)]) if type(local_ns[m.group(1)]) is dict else str(local_ns[m.group(1)]), cell_string)
+                args[2] = variable_regex.sub(
+                    lambda m: get_variable_injection_value(raw_var=m.group(1), local_ns=local_ns), cell_string)
             return func(*args, **kwargs)
         except KeyError as key_error:
             print(f'Terminated query due to undefined variable: {key_error}')
