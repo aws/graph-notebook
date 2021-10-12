e7a8cefb93891ea19d6df4f17fe0acca3e223ae9
Michael Chin <mchin188@yahoo.com>
2021-10-12 10:51:28 -0700

    Pin ipython and ipykernel dependency versions (#207)
    * Pin ipython and ipykernel dependency versions
    
    * Update Changelog
    
    Co-authored-by: Michael Chin <chnmch@amazon.com>

--- setup.py
+++ setup.py
@@ -80,9 +80,10 @@ setup(
         'requests-aws4auth==1.0.1',
         'botocore>=1.19.37',
         'boto3>=1.17.58',
-        'ipython>=7.16.1',
+        'ipython>=7.16.1,<=7.19.0',
         'neo4j==4.3.2',
-        'rdflib==5.0.0'
+        'rdflib==5.0.0',
+        'ipykernel==5.3.4'
     ],
     package_data={
         'graph_notebook': ['graph_notebook/widgets/nbextensions/static/*.js',
