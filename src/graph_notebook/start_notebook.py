9c0101e73cdb36a7177c794ec3beba67e1547459
Michael Chin <chnmch@amazon.com>
2021-10-18 14:50:05 -0700

    Fix OC query smart indent bug

--- src/graph_notebook/start_notebook.py
+++ src/graph_notebook/start_notebook.py
@@ -5,6 +5,29 @@ SPDX-License-Identifier: Apache-2.0
 
 import os
 import argparse
+import json
+
+HOME_PATH = os.path.expanduser("~")
+NOTEBOOK_CFG_PATH = HOME_PATH + '/.jupyter/nbconfig/notebook.json'
+
+
+def patch_cm_cypher_config():
+    cypher_cfg = {
+        "cm_config": {
+            "smartIndent": False,
+            "mode": "cypher"
+        }
+    }
+
+    with open(NOTEBOOK_CFG_PATH, 'a+') as file:
+        try:
+            notebook_cfg = json.load(file)
+        except json.decoder.JSONDecodeError:
+            notebook_cfg = {}
+            pass
+        notebook_cfg["CodeCell"] = cypher_cfg
+        file.seek(0)
+        json.dump(notebook_cfg, file)
 
 
 def main():
@@ -13,9 +36,10 @@ def main():
 
     args = parser.parse_args()
 
+    patch_cm_cypher_config()
+
     kernel_manager_option = "--NotebookApp.kernel_manager_class=notebook.services.kernels.kernelmanager.AsyncMappingKernelManager"
     notebooks_dir = '~/notebook/destination/dir' if args.notebooks_dir == '' else args.notebooks_dir
-
     os.system(f'''jupyter notebook {kernel_manager_option} {notebooks_dir}''')
 
 
