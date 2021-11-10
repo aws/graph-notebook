961c9efd16bcff5cb2c33c52dcb525bbd3f4e2b7
abhishek.pradeep.mishra <abhishek.pradeep.mishra@gmail.com>
2021-11-10 08:24:35 +0800

    modified doc

--- src/graph_notebook/notebooks/03-Sample-Applications/03-Identity-Graphs/neptune-glue-person.py
+++ src/graph_notebook/notebooks/03-Sample-Applications/03-Identity-Graphs/neptune-glue-person.py
@@ -32,8 +32,6 @@ job.init(args['JOB_NAME'], args)
 
 database = args['DATABASE_NAME']
 person_table = 'person'
-# product_category_table = 'salesdb_product_category'
-# supplier_table = 'salesdb_supplier'
 
 # Create Gremlin client
 
@@ -46,8 +44,6 @@ gremlin_client = GlueGremlinClient(gremlin_endpoints)
 # Create Country vertices
 # Create Person vertices
 
-# print("Creating Product vertices...")
-
 # 1. Get data from source SQL database
 datasource0 = glueContext.create_dynamic_frame.from_catalog(database = database, table_name = person_table, transformation_ctx = "datasource0")
 
@@ -55,45 +51,55 @@ datasource0 = glueContext.create_dynamic_frame.from_catalog(database = database,
 # datasource2 = datasource0.join( ["CATEGORY_ID"],["CATEGORY_ID"], datasource1, transformation_ctx = "join")
 
 # 2. Map fields to bulk load CSV column headings format
-# applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [("NAME", "string", "name:String", "string"), ("UNIT_PRICE", "decimal(10,2)", "unitPrice", "string"), ("PRODUCT_ID", "int", "productId", "int"), ("QUANTITY_PER_UNIT", "int", "quantityPerUnit:Int", "int"), ("CATEGORY_ID", "int", "category_id", "int"), ("SUPPLIER_ID", "int", "supplierId", "int"), ("CATEGORY_NAME", "string", "category:String", "string"), ("DESCRIPTION", "string", "description:String", "string"), ("IMAGE_URL", "string", "imageUrl:String", "string")], transformation_ctx = "applymapping1")
-
 
 applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [("id", "string", "id:String", "string"), 
     ("name", "string", "name:String", "string"), ("phone", "string", "phone:String", "string"), 
     ("email", "string", "email:String", "string"), ("city", "string", "city:String", "string"), ("country", "string", "country:String", "string"),
     ("pincode", "string", "pincode:String", "string"),("joineddate", "string", "joineddate:String", "string"), ("updateddate", "string", "updateddate:String", "string")], transformation_ctx = "applymapping1")
 
+# 3. create user vertices
+
 userDF = SelectFields.apply(frame = applymapping1, paths = ["id:String","name:String","joineddate:String","updateddate:String","email:String"], transformation_ctx = "userDF")
 userDF = GlueGremlinCsvTransforms.create_prefixed_columns(userDF, [('~id', 'id:String','user')])
 userDF.toDF().foreachPartition(gremlin_client.upsert_vertices('User', batch_size=100))
+
+# 4. create phone vertices
 
 phoneDF = SelectFields.apply(frame = applymapping1, paths = ["phone:String"], transformation_ctx = "phoneDF")
 phoneDF = GlueGremlinCsvTransforms.create_prefixed_columns(phoneDF, [('~id', 'phone:String','phone')])
 phoneDF.toDF().foreachPartition(gremlin_client.upsert_vertices('Phone', batch_size=100))
 
+# 5. create city vertices
+
 cityDF = SelectFields.apply(frame = applymapping1, paths = ["city:String"], transformation_ctx = "cityDF")
 cityDF = GlueGremlinCsvTransforms.create_prefixed_columns(cityDF, [('~id', 'city:String','city')])
 cityDF.toDF().foreachPartition(gremlin_client.upsert_vertices('City', batch_size=100))
 
+# 6. create country vertices
 countryDF = SelectFields.apply(frame = applymapping1, paths = ["country:String"], transformation_ctx = "countryDF")
 countryDF = GlueGremlinCsvTransforms.create_prefixed_columns(countryDF, [('~id', 'country:String','country')])
 countryDF.toDF().foreachPartition(gremlin_client.upsert_vertices('Country', batch_size=100))
+
+# 7. create user to phone edges
 
 userToPhoneMapping = SelectFields.apply(frame = applymapping1, paths = ["id:String","phone:String"], transformation_ctx = "userToPhoneMapping")
 userToPhoneMapping = GlueGremlinCsvTransforms.create_prefixed_columns(userToPhoneMapping, [('~from', 'id:String','user'),('~to', 'phone:String','phone')])
 userToPhoneMapping = GlueGremlinCsvTransforms.create_edge_id_column(userToPhoneMapping, '~from', '~to')
 userToPhoneMapping.toDF().foreachPartition(gremlin_client.upsert_edges('hasPhone', batch_size=100))
     
+# 8. create user to city edges
+
 userToCityMapping = SelectFields.apply(frame = applymapping1, paths = ["id:String","city:String"], transformation_ctx = "userToCityMapping")
 userToCityMapping = GlueGremlinCsvTransforms.create_prefixed_columns(userToCityMapping, [('~from', 'id:String','user'),('~to', 'city:String','city')])
 userToCityMapping = GlueGremlinCsvTransforms.create_edge_id_column(userToCityMapping, '~from', '~to')
 userToCityMapping.toDF().foreachPartition(gremlin_client.upsert_edges('inCity', batch_size=100))
 
+# 9. create city to country edges
+
 cityToCountryMapping = SelectFields.apply(frame = applymapping1, paths = ["city:String","country:String"], transformation_ctx = "cityToCountryMapping")
 cityToCountryMapping = GlueGremlinCsvTransforms.create_prefixed_columns(cityToCountryMapping, [('~from', 'city:String','city'),('~to', 'country:String','country')])
 cityToCountryMapping = GlueGremlinCsvTransforms.create_edge_id_column(cityToCountryMapping, '~from', '~to')
 cityToCountryMapping.toDF().foreachPartition(gremlin_client.upsert_edges('inCountry', batch_size=100))
-
 
 
 
