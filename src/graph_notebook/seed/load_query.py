87fefb6135c4660ab74d94f72ed84448719062ec
Michael Chin <chnmch@amazon.com>
2022-03-29 12:52:10 -0700

    Support individual seed file paths

--- src/graph_notebook/seed/load_query.py
+++ src/graph_notebook/seed/load_query.py
@@ -17,6 +17,18 @@ def normalize_model_name(name):
     return name
 
 
+def file_to_query(file, path_to_data_sets):
+    if file == '__init__.py' or file == '__pycache__':
+        return None
+    full_path = pjoin(path_to_data_sets, file)
+    with open(full_path, mode='r', encoding="utf-8") as f:
+        query = {
+            'name': file,
+            'content': f.read()
+        }
+        return query
+
+
 # returns a list of queries which correspond to a given query language and name
 def get_queries(model, name, location):
     if location == 'Samples':
@@ -26,17 +38,19 @@ def get_queries(model, name, location):
         path_to_data_sets = name
     queries = []
 
-    for file in os.listdir(path_to_data_sets):
-        if file == '__init__.py' or file == '__pycache__':
-            continue
-        full_path = pjoin(path_to_data_sets, file)
-        with open(full_path, mode='r', encoding="utf-8") as f:
-            query = {
-                'name': file,
-                'content': f.read()
-            }
-            queries.append(query)
-    queries.sort(key=lambda i: i['name'])  # ensure we get queries back in lexicographical order.
+    if os.path.isdir(path_to_data_sets):  # path_to_data_sets is a directory
+        for file in os.listdir(path_to_data_sets):
+            new_query = file_to_query(file, path_to_data_sets)
+            if new_query:
+                queries.append(new_query)
+        queries.sort(key=lambda i: i['name'])  # ensure we get queries back in lexicographical order.
+    else:  # path_to_data_sets is a file
+        file = os.path.basename(path_to_data_sets)
+        folder = os.path.dirname(path_to_data_sets)
+        new_query = file_to_query(file, folder)
+        if new_query:
+            queries.append(new_query)
+
     return queries
 
 
