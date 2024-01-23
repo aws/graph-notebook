5b516e53fb97eca9b837144eb7828309c35a7ddb
Michael Chin <chnmch@amazon.com>
2024-01-22 23:00:48 -0800

    Bump Jinja2 to 3.1.3

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
         'ipywidgets==7.7.2',
         'jupyterlab_widgets>=1.0.0,<3.0.0',
         'networkx==2.4',
-        'Jinja2>=3.0.3,<=3.1.2',
+        'Jinja2>=3.0.3,<=3.1.3',
         'notebook>=6.1.5,<7.0.0',
         'nbclient<=0.7.3',
         'jupyter-contrib-nbextensions<=0.7.0',
