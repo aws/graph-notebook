006178ab84ad91d0cce6d5c4d7bca40126d6dd33
Neel Shah <neeljs@amazon.com>
2026-03-05 17:23:54 -0800

    Add custom CSS support to JupyterLab launcher

--- src/graph_notebook/start_jupyterlab.py
+++ src/graph_notebook/start_jupyterlab.py
@@ -4,8 +4,18 @@ SPDX-License-Identifier: Apache-2.0
 """
 
 import os
+import shutil
 import argparse
+from pathlib import Path
 from graph_notebook.ipython_profile.configure_ipython_profile import configure_magics_extension
+
+
+def install_custom_css():
+    config_dir = Path(os.environ.get('JUPYTER_CONFIG_DIR', os.path.expanduser('~/.jupyter')))
+    dest = config_dir / 'custom'
+    dest.mkdir(parents=True, exist_ok=True)
+    src = Path(__file__).parent / 'jupyter_profile' / 'custom' / 'custom.css'
+    shutil.copy2(src, dest / 'custom.css')
 
 
 def main():
@@ -15,9 +25,10 @@ def main():
     args = parser.parse_args()
 
     configure_magics_extension()
+    install_custom_css()
 
     jupyter_dir = '~/notebook/destination/dir' if args.jupyter_dir == '' else args.jupyter_dir
-    os.system(f'''jupyter lab {jupyter_dir}''')
+    os.system(f'''jupyter lab --custom-css {jupyter_dir}''')
 
 
 if __name__ == '__main__':
